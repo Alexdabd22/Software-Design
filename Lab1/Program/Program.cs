8413872e31db73fc7df8378814e8fdb1353fbc60
Pastukhov Aleksandr <ipz224_pok@student.ztu.edu.ua>
2024-03-09 22:12:52 +0200

    Добавлено UML діаграму

--- Lab1/Program/Program.cs
+++ Lab1/Program/Program.cs
@@ -13,11 +13,11 @@ namespace Program
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            // Ініціалізація валют
+
             Dollar dollar = new Dollar(0, 0);
             Euro euro = new Euro(0, 0);
 
-            // Створення продуктів
+ 
             Product PC = new Product("Комп'ютер", new Money(new Dollar(500, 0)), "Електронiка");
             Product Phone = new Product("Телефон", new Money(new Euro(10, 50)), "Електронiка");
             Product Tea = new Product("Зелений чай", new Money(new Euro(5, 25)), "Напої");
@@ -29,40 +29,40 @@ namespace Program
 
             List<Product> products = new List<Product> { PC, Phone, Tea, Coffee };
 
-            // Виведення інформації про всі продукти
+
             Console.WriteLine("Всi продукти:");
             foreach (var product in products)
             {
                 Console.WriteLine($"{product.Name} -- {product.Category} -- Цiна: {product.Price}");
             }
-            // Ініціалізація складу
+        
             Warehouse warehouse = new Warehouse();
 
-            // Ініціалізація модуля звітності
+           
             Reporting reporting = new Reporting(warehouse);
 
-            // Реєстрація прибуття товарів на склад
+            
             Console.WriteLine("\nРеєстрацiя надходження товару:");
             reporting.RegisterArrival(new WarehouseItem(PC, 10, DateTime.Now));
             reporting.RegisterArrival(new WarehouseItem(Phone, 100, DateTime.Now));
             reporting.RegisterArrival(new WarehouseItem(Tea, 120, DateTime.Now));
             reporting.RegisterArrival(new WarehouseItem(Coffee, 80, DateTime.Now));
 
-            // Виведення звіту по інвентаризації
+           
             Console.WriteLine("\nЗвiт про iнвентаризацiю:");
             foreach (var item in reporting.InventoryReport())
             {
                 Console.WriteLine($"{item.Product.Name}: {item.Quantity} одиниць");
             }
 
-            // Реєстрація відвантаження товару
+           
             Console.WriteLine("\nОформлення вiдправлення товару:");
             reporting.RegisterShipment("Комп'ютер", 2);
             reporting.RegisterShipment("Телефон", 50);
             reporting.RegisterShipment("Зелений чай", 60);
             reporting.RegisterShipment("Кава", 65);
 
-            // Виведення оновленого звіту по інвентаризації
+            
             Console.WriteLine("\nОновлений звiт про iнвентаризацiю:");
             foreach (var item in reporting.InventoryReport())
             {
@@ -74,7 +74,7 @@ namespace Program
             {
                 Console.WriteLine($"{product.Name} -- Цiна: {product.Price}");
             }
-            // Виведення всіх накладних
+            
             Console.WriteLine("\nВсi рахунки-фактури:");
             reporting.PrintAllInvoices();
 
