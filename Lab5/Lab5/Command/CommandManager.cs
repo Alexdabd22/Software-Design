b41140cb82e8063b87a22d9057a23362add1fee2
Pastukhov Aleksandr <ipz224_pok@student.ztu.edu.ua>
2024-05-04 16:28:46 +0300

    Шаблон Command

---
+++ Lab5/Lab5/Command/CommandManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Command
+{
+    public class CommandManager
+    {
+        private readonly Stack<ICommand> executedCommands = new Stack<ICommand>();
+
+        public void ExecuteCommand(ICommand command)
+        {
+            command.Execute();
+            executedCommands.Push(command);
+        }
+
+        public void UndoLastCommand()
+        {
+            if (executedCommands.Count > 0)
+            {
+                var command = executedCommands.Pop();
+                command.Undo();
+            }
+        }
+    }
+}
