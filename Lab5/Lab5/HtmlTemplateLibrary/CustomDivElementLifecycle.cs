45594dea3a679c450c67f6bb0afa1755d07a15c5
Pastukhov Aleksandr <ipz224_pok@student.ztu.edu.ua>
2024-05-04 20:06:32 +0300

    Шаблон TemplateMethod

---
+++ Lab5/Lab5/HtmlTemplateLibrary/CustomDivElementLifecycle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace HtmlTemplateLibrary
+{
+    public class CustomDivElementLifecycle : HtmlElementLifecycle
+    {
+        protected override void OnCreated() => Console.WriteLine("Див створено.");
+        protected override void ApplyStyles() => Console.WriteLine("Див стилізовано особливим чином.");
+    }
+}
---
+++ Lab5/Lab5/HtmlTemplateLibrary/HtmlElementLifecycle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace HtmlTemplateLibrary
+{
+    public abstract class HtmlElementLifecycle
+    {
+        public void Render()
+        {
+            OnCreated();
+            OnInserted();
+            ApplyStyles();
+            ApplyClassList();
+            OnTextRendered();
+            Console.WriteLine("Рендеринг завершено.");
+        }
+
+        protected virtual void OnCreated() => Console.WriteLine("Елемент створено.");
+        protected virtual void OnInserted() => Console.WriteLine("Елемент вставлено у DOM.");
+        protected virtual void ApplyStyles() => Console.WriteLine("Стилі застосовано.");
+        protected virtual void ApplyClassList() => Console.WriteLine("Список класів застосовано.");
+        protected virtual void OnTextRendered() => Console.WriteLine("Текст відтворено.");
+    }
+}
