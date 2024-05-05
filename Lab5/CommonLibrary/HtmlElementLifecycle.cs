70fe3c17a4f0987b8f238deb00208a170e8372c2
Pastukhov Aleksandr <ipz224_pok@student.ztu.edu.ua>
2024-05-05 15:43:10 +0300

    Доповнено TemplateMethod

---
+++ Lab5/CommonLibrary/HtmlElementLifecycle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace CommonLibrary
+{
+    public abstract class HtmlElementLifecycle
+    {
+        // Шаблонний метод рендерингу
+        public void Render()
+        {
+            OnCreated();
+            OnInserted();
+            ApplyStyles();
+            ApplyClassList();
+            OnTextRendered();
+            Console.WriteLine("Рендеринг завершено.");
+        }
+
+        public virtual void OnCreated() => Console.WriteLine("Елемент створено.");
+        public virtual void OnInserted() => Console.WriteLine("Елемент вставлено у DOM.");
+        public virtual void ApplyStyles() => Console.WriteLine("Стилі застосовано.");
+        public virtual void ApplyClassList() => Console.WriteLine("Список класів застосовано.");
+        public virtual void OnTextRendered() => Console.WriteLine("Текст відтворено.");
+    }
+}
---
+++ Lab5/TemplateMethod/CustomSpanElementLifecycle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace TemplateMethod
+{
+    public class CustomSpanElementLifecycle : CommonLibrary.HtmlElementLifecycle
+    {
+        public override void OnCreated() => Console.WriteLine("Спан створено.");
+        public override void ApplyClassList() => Console.WriteLine("Спан отримав клас 'highlight'.");
+    }
+}
