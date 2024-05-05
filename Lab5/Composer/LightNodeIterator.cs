06e989ff51e377ca5e9f3989237ef4658078e4c4
Pastukhov Aleksandr <ipz224_pok@student.ztu.edu.ua>
2024-05-05 12:12:45 +0300

    Шаблон Iterator

---
+++ Lab5/Composer/LightNodeIterator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Composer
+{
+    public class LightNodeIterator : IEnumerator<LightNode>
+    {
+        private readonly Stack<LightNode> nodeStack = new Stack<LightNode>();
+        private LightNode currentNode;
+
+        public LightNodeIterator(LightNode rootNode)
+        {
+            nodeStack.Push(rootNode);
+        }
+
+        public LightNode Current => currentNode;
+
+        object IEnumerator.Current => Current;
+
+        public bool MoveNext()
+        {
+            if (nodeStack.Count == 0) return false;
+
+            currentNode = nodeStack.Pop();
+
+            if (currentNode is LightElementNode elementNode)
+            {
+                int count = elementNode.Children.Count;
+                int index = count - 1;
+
+                while (index >= 0)
+                {
+                    nodeStack.Push(elementNode.Children[index]);
+                    index--;
+                }
+            }
+
+            return true;
+        }
+
+        public void Reset() => throw new NotImplementedException();
+
+        public void Dispose() { }
+    }
+}
