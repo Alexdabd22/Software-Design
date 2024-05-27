0526a68bcdf86fc8a01f03d8fb722e986b4dc5ac
Костянтин Гольцев <ipz224_gko@student.ztu.edu.ua>
2024-05-27 16:32:46 +0300

    Added singleton class to get db connection

---
+++ Lab6/TicTacToeGame/TicTacToeDataAccess/DatabaseConnection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+
+
+namespace TicTacToeDataAccess
+{
+    public sealed class DatabaseConnection
+    {
+        private static readonly object lockObj = new object();
+        private static DatabaseConnection _instance;
+        private SQLiteConnection connection;
+
+        private DatabaseConnection()
+        {
+            string databasePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TicTacToeGame.db");
+            if (!File.Exists(databasePath))
+            {
+                SQLiteConnection.CreateFile(databasePath);
+            }
+
+            connection = new SQLiteConnection($"Data Source={databasePath};Version=3;");
+            connection.Open();
+        }
+
+        public static DatabaseConnection Instance
+        {
+            get
+            {
+                lock (lockObj)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new DatabaseConnection();
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        public SQLiteConnection GetConnection()
+        {
+            return connection;
+        }
+    }
+
+}
